d48a26d0a938178d7ac9a0ecc371c14e969a4bb3
void <git.email.address@googlemail.com>
2011-06-28 14:59:18 +0200

    Works

---
+++ replace.rb
@@ -0,0 +1,32 @@
+require 'nil/file'
+
+def processDirectory(path, target, replacement)
+  targets = Nil.readDirectory(path, true)
+  if targets == nil
+    raise "Unable to read path #{path}"
+  end
+  directories, files = targets
+  directories.each do |entry|
+    processDirectory(entry.path, target, replacement)
+  end
+
+  files.each do |entry|
+    path = entry.path
+    input = Nil.readFile(path)
+    output = input.gsub(target, replacement)
+    if input != output
+      puts "Replaced a string in #{path}"
+      Nil.writeFile(path, output)
+    end
+  end
+end
+
+if ARGV.size != 2
+  puts '<target> <replacement>'
+  exit
+end
+
+target = Regexp.new(ARGV[0])
+replacement = ARGV[1]
+
+processDirectory('.', target, replacement)
